11ee2596a58bf2324c17f3578cea225dbba4a4fa
Martin <jmartinnu@gmail.com>
2023-03-13 20:01:04 -0300

    cpp juegoMesa

--- src/JuegoMesa.cpp
+++ src/JuegoMesa.cpp
@@ -1,2 +1,34 @@
 #include "JuegoMesa.h"
 
+JuegoMesa::JuegoMesa(){
+    this->CantJugadores = 0;
+    this->EdadRecomendada = 0;
+}
+
+JuegoMesa::JuegoMesa(int edad, int cantidad){
+    this->CantJugadores = cantidad;
+    this->EdadRecomendada = edad;
+}
+
+JuegoMesa::~JuegoMesa(){
+}
+
+void JuegoMesa::setEdadRecomendada(int edad){
+    this->EdadRecomendada = edad;
+}
+
+void JuegoMesa::setCantJugadores(int cant){
+    this->EdadRecomendada = cant;
+}
+
+int JuegoMesa::getEdadRecomendada(){
+    return this->EdadRecomendada;
+}
+
+int JuegoMesa::getCantJugadores(){
+    return this->CantJugadores;
+}
+
+string JuegoMesa::toString(){
+    
+}
