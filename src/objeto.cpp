ddea2429d2038f468ee7ae570b42498aa8922963
Juanicab9 <juanicab9@gmail.com>
2023-03-13 19:25:13 -0300

    primer cambio

---
+++ src/objeto.cpp
@@ -0,0 +1,23 @@
+#include <string> 
+using namespace std;     
+
+class Objeto
+{
+private:
+    string Nombre;
+    int AnioComprado;
+    Estado{
+        nuevo,bienConservado,roto
+    }
+public:
+    objeto();
+    ~objeto();
+};
+
+objeto::objeto(/* args */)
+{
+}
+
+objeto::~objeto()
+{
+}
