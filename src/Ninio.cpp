19844942d7708b859a3bfc0272dc25b6542800c7
ange <angelina.machuca@fing.edu.uy>
2023-03-13 20:09:47 -0300

    mas

--- src/Ninio.cpp
+++ src/Ninio.cpp
@@ -3,26 +3,40 @@
 string *Ninio::ListarObjetosPrestados() {
 }
 
+Ninio::Ninio() {  //constructor
+}
+
+Ninio::~Ninio() {   //desructor
+}
+
 string Ninio::getnombre() {
+    return nombre;
 }
 
 void Ninio::setnombre(string nombre) {
+    this->nombre=nombre;
 }
 
 string Ninio::getdireccion() {
+    return direccion;
 }
 
 void Ninio::setdireccion(string direccion) {
+    this->direccion=direccion;
 }
 
 string Ninio::gettelefono() {
+    return telefono;
 }
 
 void Ninio::settelefono(string telefono) {
+    this->telefono=telefono;
 }
 
 string Ninio::getedad() { 
+    return edad;
 }
 
 void Ninio::setedad(int edad) {
+    this->edad=edad;
 }
