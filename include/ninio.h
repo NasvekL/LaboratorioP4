392bf1ec20a9d2a79095c9096c5235990bbf8586
ange <angelina.machuca@fing.edu.uy>
2023-03-13 19:12:43 -0300

    ninio.h

---
+++ include/ninio.h
@@ -0,0 +1,18 @@
+#include <string>;
+
+
+
+class ninio {
+    private:
+        string nombre;
+        int edad;
+        string direccion;
+        string telefono;
+    public:
+        string setListarObjetosPrestados();
+
+
+};
+
+string ninio::setListarObjetosPrestados(){
+};
