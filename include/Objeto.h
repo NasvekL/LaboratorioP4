3d1e013c2402e8b10099a51a2e5f73ac2719dd76
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2023-03-13 21:01:54 -0300

    estado

--- include/Objeto.h
+++ include/Objeto.h
@@ -9,17 +9,17 @@ class Objeto
 private:
     string Nombre;
     int AnioComprado;
-    estado estado;
+    Estado estado;
     Ninio *prestadoA;
 public:
     virtual string toString() = 0;
     //getters y setters 
     void setNombre(string);
     void setAnioComprado(int);
-    void setEstado(estado);
+    void setEstado(Estado);
     string getNombre();
     int getAnioComprado();
-    estado getEstado();
+    Estado getEstado();
 };
 
 
