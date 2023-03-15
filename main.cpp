e5fe5a388a046e83fafb17797f28614bae268bf9
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2023-03-15 20:24:27 -0300

    manin tabs arreglados

--- main.cpp
+++ main.cpp
@@ -7,16 +7,14 @@
 
 int main(){
     //Parte a)   (sin salida por consola)
-    Libro libro1("Beandon Sanderson", 688, "Nacidos de la bruma: El imperio final", 2022, roto);
-    Libro libro2("Camila Sosa Villada", 240, "Las Malas", 2022, nuevo);
-    Libro libro3("Gemma Merino", 32, "El cocodrilo al que no le gustaba el agua", 2016, roto);
+        Libro* libro1 = new Libro("Beandon Sanderson", 688, "Nacidos de la bruma: El imperio final", 2022, roto);
+        Libro* libro2 = new Libro("Camila Sosa Villada", 240, "Las Malas", 2022, nuevo);
+        Libro* libro3 = new Libro("Gemma Merino", 32, "El cocodrilo al que no le gustaba el agua", 2016, roto);
+
     //Parte b)   (sin salida por consola)
-
-    Objeto* j1 = new JuegoMesa(7, 10, "Juego Uno", 2022, roto);
-    Objeto* j2 = new JuegoMesa(7, 4, "Mazo de Cartas", 2019, nuevo);
-    Objeto* j3 = new JuegoMesa(2, 6, "Dados", 2020, roto);
-
-
+        Objeto* j1 = new JuegoMesa(7, 10, "Juego Uno", 2022, roto);
+        Objeto* j2 = new JuegoMesa(7, 4, "Mazo de Cartas", 2019, nuevo);
+        Objeto* j3 = new JuegoMesa(2, 6, "Dados", 2020, roto);
 
     cout << "Parte c)" << endl;
 
@@ -27,17 +25,15 @@ int main(){
             Ninio *n2 = new  Ninio("Alex", 5, "Humberto Primo 1501",  "29094141");
 
     cout << "Parte e)" << endl;
-    n1->agregar_objeto(j2);
-    n1->agregar_objeto(&libro1);
-    n1->agregar_objeto(j3);
-    n2->agregar_objeto(j1);
-    n2->agregar_objeto(&libro3);
-
+        n1->agregar_objeto(j2);
+        n1->agregar_objeto(libro1);
+        n1->agregar_objeto(j3);
+        n2->agregar_objeto(j1);
+        n2->agregar_objeto(libro3);
 
     cout << "Parte f)" << endl;
-    n1->ListarObjetosPrestados();
-    n2->ListarObjetosPrestados();
-
+        n1->ListarObjetosPrestados();
+        n2->ListarObjetosPrestados();
 
     cout << "Parte g)" << endl;
 
