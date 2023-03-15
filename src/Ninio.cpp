1b6970ac75e0b2023adf35899f0affea22584bb8
ange <angelina.machuca@fing.edu.uy>
2023-03-15 19:55:54 -0300

    g

--- src/Ninio.cpp
+++ src/Ninio.cpp
@@ -45,10 +45,12 @@ int Ninio::getedad() {
 void Ninio::setedad(int edad) {
     this->edad=edad;
 }
+
 void Ninio:: agregar_objeto(Objeto* objeto){
     objetos_.push_back(objeto);
     objeto->setPrestadoA(this);
-    }
+}
+
 list<Objeto*>& Ninio:: objetos(){
     return objetos_;
 }
