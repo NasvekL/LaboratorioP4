cf5ecdd32a5e034a775f3d3c710fbd0d480b1601
Martin <jmartinnu@gmail.com>
2023-03-15 20:59:51 -0300

    .

--- main.cpp
+++ main.cpp
@@ -45,10 +45,9 @@ int main(){
 
 
     cout << "Parte h)" << endl;
-   
-    Ninio* n_prestado = j1->getPrestadoA();         //n_prestado tiene que ser un puntero?
-    n_prestado->eliminar_Robjeto(j1); 
-    j1->~JuegoMesa();          
+        Ninio* n_prestado = j1->getPrestadoA();         //n_prestado tiene que ser un puntero?
+        n_prestado->eliminar_Robjeto(j1); 
+        j1->~JuegoMesa();          
 
 
 
