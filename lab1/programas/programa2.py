ffb64526c733b63708e6671f716ea3defa0dddc7
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2023-04-13 17:36:02 -0300

    programa 2 listo

--- lab1/programas/programa2.py
+++ lab1/programas/programa2.py
@@ -3,10 +3,9 @@ import re
 import sys
 
 def prog(texto):
-    #
-    
-    return ""
-
+    match = re.findall(r'"tag": "((.*)(ent|ing|or|on))",', texto)
+    resultado = " ".join([m[0] for m in match])
+    return resultado
 if __name__ == '__main__':
     entrada = sys.argv[1]  # archivo entrada (param)
     salida = sys.argv[2]   # archivo salida (param)  jkhkjhjk
