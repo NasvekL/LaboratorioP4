cf17f396a72f7a59ef1a9a4e3f976ede5a79b6e3
Julio <julio.gu@fing.edu.uy>
2023-04-17 17:09:42 -0300

    hola

--- lab1/programas/programa5.py
+++ lab1/programas/programa5.py
@@ -3,9 +3,21 @@ import re
 import sys
 
 def prog(texto):
-    #
-    
-    return ""
+    match = re.findall(r'"tag": "(.*)",(?:\n| )*"patterns": \[((?:(?:\n| )*".*",)*(?:\n| )*".*")(?:\n| )*\],(?:\n| )*"responses": \[((?:(?:\n| )*".*",)*(?:\n| )*".*")', texto, re.MULTILINE)
+    #                                    0(  )                        1(                                    )                                 
+    diccionario1 = {}
+    dic2 = {}
+    i=0
+    p=0
+    r=0
+    for m in match:
+        patterns = re.findall(r'"(.*)"', m[1], re.MULTILINE)
+        responses = re.findall(r'"(.*)"',m[2],re.MULTILINE)
+        p+=len(patterns)
+        r+=len(responses)
+        i+=1
+    resultado = str(i) + " " + str(p) + " " + str(r)
+    return resultado
 
 if __name__ == '__main__':
     entrada = sys.argv[1]  # archivo entrada (param)
