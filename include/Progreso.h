834f869830c9a36171563d1ca55e5771cc4ab947
Julio <julio.gu@fing.edu.uy>
2023-05-31 22:12:03 -0300

    agregue dts y corregi calsesssssss

--- include/Progreso.h
+++ include/Progreso.h
@@ -1,18 +1,27 @@
 #ifndef PROGRESO
 #define PROGRESO
 #include "Utils.h"
-
+#include "Ejercicio.h"
+#include "Leccion.h"
+#include "Inscripcion.h"
 
 class Progreso{
     private:
         int porcentaje;
         //Pseudoatributos
-        //inscripcion : curso
-        //leccion: leccion
-        //ejercicios: set<ejercicio>
+        Inscripcion* curso
+        Leccion* leccion
+        ejercicios set<ejercicio>
     public:
         virtual bool esCorrecto(string solucion);
         void aumentarProgreso(string estudiante);
+        //setters
+        void setPorcentaje(int porcentaje);
+        //getters
+        int getPorcentaje();
+        //constructor destructor
+        Progreso(int Porcentaje);
+        ~Progreso();
 };
 
 
