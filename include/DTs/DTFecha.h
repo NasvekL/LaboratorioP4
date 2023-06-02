ad808a33c78b165990ee5ba14584983ac9bf0e3b
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2023-06-02 12:57:51 -0300

    moviendo todo para que quede prolijo

---
+++ include/DTs/DTFecha.h
@@ -0,0 +1,31 @@
+#ifndef DTFECHA
+#define DTFECHA
+#include "Utils.h"
+
+class DTFecha{
+
+    private:
+        int dia;
+        int mes;
+        int anio;
+
+
+    public:
+        int getDia();
+        int getMes();
+        int getAnio();
+        string toString();
+
+        ~DTFecha();
+        DTFecha(int dia, int mes, int anio);
+};
+#endif;
+
+
+
+
+
+
+
+
+
