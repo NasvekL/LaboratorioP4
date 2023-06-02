1a0fec5ed3b72d5eb94698051575dc52da2b78a8
Nahuel Lopez <nahuel.lopez.areco@fing.edu.uy>
2023-06-02 05:17:41 -0300

    n

--- include/Curso.h
+++ include/Curso.h
@@ -15,32 +15,44 @@ class Curso{
         string nombre;
         string descripcion;
         nivelDeDificultad nivelDeDif;
-        bool Habilitado;
+        bool habilitado;
         set<DTCurso> previas; 
         //Pseudoatributos
-        Idioma* seDictaEn;
-        Profesor* loDicta;
+        Idioma* idiomaDelCurso;
+        Profesor* profesorQueLoDicta;
         set<Leccion*> lecciones;
         set<Inscripcion*> inscripciones;
     public:
-        //Getters
+        //CONSTRUCTOR Y DESTRUCTOR
+        ~Curso();
+        Curso(string nombre, string descripcion, nivelDeDificultad nivel, bool habilitado, set<DTCurso> previas);
+
+        //GETTERS
         string getNombreCurso();
         string getDescripcion();
         nivelDeDificultad getNivelDeDificultad();
         bool getHabilitado();
-        //Create y destroy
-        ~Curso();
-        Curso(string nombre, string descripcion, nivelDeDificultad nivel, bool habilitado, set<DTCurso> previas);
-        //Otras operaciones
-        Ejercicio seleccionar(int idEjercicio);
-        int getPromedio(Profesor p);
-        int getProgreso(Estudiante e);
-        DTestadisticaCurso mostrarEstadisticas();
+        //SETTERS
+        void setHabilitado(bool habilitado);
+
+        //GETTERS DE PSEUDOATRIBUTOS
+        Idioma* getIdiomaDelCurso();
+        Profesor* getProfesorQueLoDicta();
+        set<Leccion*>& getLecciones(); 
+        set<Inscripcion*>& getInscripciones();
+        //SETTERS DE PSEUDOATRIBUTOS
+        void setIdiomaDelCurso(Idioma* i);
+        void setProfesorQueLoDicta(Profesor* p);
+        void setLecciones(set<Leccion*> lecciones);
+        void setInscripciones(set<Inscripcion*> inscripciones);
+
+        //OPERACIONES
+        Ejercicio seleccionarEjercicio(int idEjercicio); //Esto iria en controladorCurso me parece
+        int progresoPromedio();
+        int progresoDeEstudiante(Estudiante e);
+        DTestadisticaCurso estadisticasCurso();
+
         //Obtener Pseudoatributos
-        Idioma* idiomas();
-        Profesor* profesores();
-        set<Leccion*>& lecciones(); 
-        set<Inscripcion*>& inscripciones();
         //Faltan los casos de uso que nos den las operaciones de agregar idiomas y cursos
 };
 
