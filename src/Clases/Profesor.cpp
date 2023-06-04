e6c39b3e33bf58944c378f694321975af7e554c9
Juanicab9 <juanicab9@gmail.com>
2023-06-04 19:26:02 -0300

    d

--- src/Clases/Profesor.cpp
+++ src/Clases/Profesor.cpp
@@ -1,4 +1,33 @@
 #include "../Clases/Profesor.h"
 
 
-void Profesor
+void Profesor:: Profesor(string nickname, string nombre, string contrasenia, string descripcion, string instituto,list<idiomas*> idiomas){
+    this->nickname = nickname;
+    this->nombre = nombre;
+    this->contrasenia = contrasenia;
+    this->descripcion = descripcion;
+    this->instituto = instituto;
+    while idiomas.size() != 0{
+        this->idiomas.push_back(idiomas.front());
+        idiomas.pop_front();
+    } 
+};
+
+void Pofesor:: ~Profesor(){
+
+};
+
+string Profesor:: getInstituto(){
+    return this-> instituto;
+};
+
+list<DTNotificacion> Profesor:: verNotificaciones(){
+        for (auto i : this->notifiaciones) {
+		cout << i << ' ';
+	}
+	return 0;
+};
+
+DTProfesor Profesor:: getDTProfesor(){
+    
+}
