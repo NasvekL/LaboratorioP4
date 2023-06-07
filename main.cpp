aacdec44e7adc4f52b249d5919b412f103c7fb8e
Juani <juanicab9@gmail.com>
2023-06-07 12:53:08 -0300

    gg

--- main.cpp
+++ main.cpp
@@ -58,9 +58,9 @@ int main(){
     //la interfaz es abstracta, entonces no se puede crear una instancia de la interfaz
     //Ergo utilidadInterfaz = 0?
     //La gracia es que cambias el controlador y no la interfaz, no tiene que ver lo que toques en el main
-    IControladorCurso* interfazCurso = ControladorCurso::getInstancia();
-    IControladorUsuario* interfazUsuario = ControladorUsuario::getInstancia();
-
+    factoryController* Fabrica = factoryController::getFactoryInstance();
+    IControladorUsuario* Iuser = Fabrica->getIControladorUsuario();
+    IControladorCurso* Icurso = Fabrica->getIControladorCurso();
 
     int opcion = 1;
     while(opcion != 0){
@@ -74,11 +74,11 @@ int main(){
                 int opcion;
                 opcion = seleccionEstudianteOProfesor();
                 switch (opcion){
-                    //factoryController Fabrica = factoryController::getFactoryInstance();
-                    //IControladorUsuario* Iuser = Fabrica.getIControladorUsuario();
+                    factoryController* Fabrica = factoryController::getFactoryInstance();
+                    IControladorUsuario* Iuser = Fabrica->getIControladorUsuario();
                     case 1:{
                     DTEstudiante est = crearDTEstudiante();
-                    //Iuser->guardarDatosEstudiante(est);
+                    Iuser->guardarDatosEstudiante(est);
                     cout << VERDE << "Estudiante creado" << RESET << endl;
                     esperar(3);
                     break;
