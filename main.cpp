0e8d35e9f61dffa0275b5ae7b293c13a5d896e70
Juani <juanicab9@gmail.com>
2023-06-07 14:34:14 -0300

    dadsdasd

--- main.cpp
+++ main.cpp
@@ -58,9 +58,9 @@ int main(){
     //la interfaz es abstracta, entonces no se puede crear una instancia de la interfaz
     //Ergo utilidadInterfaz = 0?
     //La gracia es que cambias el controlador y no la interfaz, no tiene que ver lo que toques en el main
-    factoryController* Fabrica = factoryController::getFactoryInstance();
-    IControladorCurso& ContCurso = Fabrica->getIControladorCurso();
-    IControladorUsuario& ContUsuario = Fabrica->getIControladorUsuario();
+    factoryController& Fabrica = factoryController::getInstancia();
+    IControladorCurso& ContCurso = Fabrica.getIControladorCurso();
+    IControladorUsuario& ContUsuario = Fabrica.getIControladorUsuario();
     int opcion = 1;
 while(opcion != 0){
     opcion = menuPrincipal();
@@ -73,13 +73,10 @@ while(opcion != 0){
             int seleccion = seleccionEstudianteOProfesor();
             switch (seleccion){
                 case 1:{
-                    factoryController* Fabrica = factoryController::getFactoryInstance();
-                    IControladorUsuario& Iuser = Fabrica->getIControladorUsuario();
                     DTEstudiante est = crearDTEstudiante();
-                    Iuser.guardarDatosEstudiante(est);
+                    ContUsuario.guardarDatosEstudiante(est);
                     cout << VERDE << "Estudiante creado" << RESET << endl;
                     esperar(3);
-                    Iuser.
                     break;
                 }
                 case 2:{
@@ -235,13 +232,6 @@ DTProfesor crearDTProfesor(){
     cout << "Ingrese instituto de profesor:" << endl;
     string instituto;
     cin >> instituto;
-    factoryController* Fabrica = factoryController::getFactoryInstance();
-    IControladorUsuario& Iuser = Fabrica->getIControladorUsuario();
-    list<string> idiomas = Iuser.listarIdiomas();
-    for(int i=0; i<idiomas.size(); i++){
-    
-    }
-    //TO BE CONTINUED
 }
 
 //verificar que entrada sea un int
