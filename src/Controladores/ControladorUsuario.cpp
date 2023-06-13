61c7de1d107153dffcca872a611d2afd2e1f28a8
Juani <juanicab9@gmail.com>
2023-06-13 11:14:12 -0300

    dd

--- src/Controladores/ControladorUsuario.cpp
+++ src/Controladores/ControladorUsuario.cpp
@@ -152,9 +152,11 @@ void ControladorUsuario::confirmarAltaUsuario() {
     // Implementación de la función confirmarAltaUsuario
     ControladorCurso& cc = ControladorCurso::getInstancia();
     if (datoEstudiante!=NULL){
-        map<string,Usuario>* users = getUsuarios();
+        map<string,Usuario> *users = getUsuarios();
         Estudiante e = Estudiante(datoEstudiante->getNickname(),datoEstudiante->getContrasenia(), datoEstudiante->getNombre(),datoEstudiante->getDescripcion(),datoEstudiante->getPais(),datoEstudiante->getNacimiento());
+        cout<<"puto"<<endl;
         users->insert(pair<string, Usuario>(datoEstudiante->getNickname(), e));
+        cout<<"hola"<<endl;
         delete datoEstudiante;
         datoEstudiante = NULL;
     }
