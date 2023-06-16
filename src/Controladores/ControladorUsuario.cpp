b02f80ced9496b5a816877fc5a1982e1aa63f69d
Julio <julio.gu@fing.edu.uy>
2023-06-16 15:36:21 -0300

    fd

--- src/Controladores/ControladorUsuario.cpp
+++ src/Controladores/ControladorUsuario.cpp
@@ -95,7 +95,7 @@ list<DTEstudianteSC> ControladorUsuario::listarEstudiantes() {
         string tipoUsuario = getTipoUsuario(usuario.first);
         if (tipoUsuario == "estudiante") {
             Estudiante* estudiante = dynamic_cast<Estudiante*>(usuario.second);
-            DTEstudianteSC estu =  DTEstudianteSC(estudiante->getNickname(), estudiante->getNombre(), estudiante->getDescripcion(), estudiante->getPais() , estudiante->getNacimiento());
+            DTEstudianteSC estu =  DTEstudianteSC(estudiante->getNick(), estudiante->getNombre(), estudiante->getDescripcion(), estudiante->getPais() , estudiante->getNacimiento());
             estudiantes.push_back(estu);
         }
     }
